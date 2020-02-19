71e34ab2a5d4ee96c860c6eaffe3992b186e0aa7
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-02-19 18:23:02 -0500

    changed to spago infrastructure

--- my-project/src/Main.purs
+++ my-project/src/Main.purs
@@ -1,11 +1,10 @@
 module Main where
 
 import Prelude
-import Effect.Console
-import Math (sqrt,pi)
 
-main = logShow (diagonal 3.0 4.0)
+import Effect (Effect)
+import Effect.Console (log)
 
-diagonal w h = sqrt (w * w + h * h)
-
-circleArea r = pi * (sqrt r)
+main :: Effect Unit
+main = do
+  log "🍝"
